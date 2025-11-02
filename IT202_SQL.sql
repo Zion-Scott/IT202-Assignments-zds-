f74ded9ca9c44a36e8e090fbd88c19b58d17c014
Zion <zds@njit.edu>
2025-11-02 18:28:34 -0500

    Week 9 Participation

--- IT202_SQL.sql
+++ IT202_SQL.sql
@@ -23,3 +23,36 @@ UPDATE `SampleTable` SET `Address` = '495 Updated Rd.' WHERE `SSN` = '987654321'
 -- a DELETE statement that deletes record(s) using a specific column(s)  for a match on a specific value in your DB table
 DELETE FROM `SampleTable` WHERE `SSN` = '104857384';
 
+--Create new tables | Week 7 Assignment
+
+SELECT * FROM `Transcript`; 
+SELECT * FROM `Student`; 
+DELETE FROM `Transcript` WHERE `ID` = '';
+
+
+CREATE TABLE Student (
+    FirstName VARCHAR(50),
+    ID INT PRIMARY KEY,
+    Major VARCHAR(50)
+);
+
+CREATE TABLE Transcript (
+    ID INT PRIMARY KEY,
+    Course VARCHAR(10),
+    Grade VARCHAR(2)
+);
+
+INSERT INTO `Transcript` (`ID`, `Course`, `Grade`)
+VALUES ('', '', '');
+
+--Join Student table with Transcrip table
+SELECT 
+  Student.ID AS ID,
+  Student.FirstName,
+  Student.Major,
+  Transcript.Course,
+  Transcript.Grade
+FROM Student
+INNER JOIN Transcript
+ON Student.ID = Transcript.ID;
+
---
+++ Week9_Participation/ErrorFinding.php
@@ -0,0 +1,19 @@
+<?php //Error #1 Incorrect opening tag for php
+
+require('connect.php'); // Error #2 No quotes around connect.php | Error #4 missing semicolon after require statement
+// This code has 10 errors. Find and fix them. Error #4 Incorrect comment tag
+$name = $_POST['name'];
+if (isset ($name)) { //Error #5 Incorrect variable name ($name and $Name) | Error #6 missing closing parenthesis
+ //Error #7 No closing curly bracket 
+$sql = "SELECT * FROM 'Client Table' WHERE Name='$name' "; 
+$result = mysqli_query($con, $sql); //Error #8 Missing semicolon after defining result variable
+$rows = mysql_num_rows($result);
+}
+
+if($rows == 1) //Error #9 Using assignment operand (=)instead of comparison (==) operand
+{
+echo $name; //Error #10 Forgot to add $ for $name variable
+} 
+
+?>
+
